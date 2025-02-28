7eaa5987f39631fa4a86ab9a7179d86a8a58aad4
Daniel Kandiah <kandiahd@sheridancollege.ca>
2025-02-28 18:51:10 -0500

    made game controller and simple html pages

---
+++ src/main/java/sheridan/daniel/assignmentone/controller/GameController.java
@@ -0,0 +1,27 @@
+package sheridan.daniel.assignmentone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sheridan.daniel.assignmentone.model.Game;
+
+@Controller
+public class GameController {
+
+    @GetMapping("/")
+    public String showGamePage() {
+        return "game";
+    }
+
+    @GetMapping("/play")
+    public String playGame(@RequestParam("box") int boxIndex, Model model) {
+        Game game = new Game();
+        game.setUserPick(boxIndex);
+
+        model.addAttribute("userPick", game.getUserPick());
+        model.addAttribute("prize", game.getPrize());
+
+        return "result";
+    }
+}
